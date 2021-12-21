9da4b580c1df8493552a160dd61f8fe9edf9d883
Alex Antonides <alex.m.antonides@gmail.com>
2021-12-21 17:11:30 +0100

    New ensemble implementation

--- ensemble.py
+++ ensemble.py
@@ -1,36 +1,8 @@
 import torch
+from torch import Tensor
 
 from kge import Config, Dataset
 from kge.model.kge_model import KgeEmbedder, KgeModel, RelationalScorer
-
-from kge.model.complex import ComplExScorer
-
-class EnsembleScorer(RelationalScorer):
-    def __init__(self, config: Config, dataset: Dataset, configuration_key=None):
-        super().__init__(config, dataset, configuration_key)
-        self.models = []
-
-    def score_emb(
-        self,
-        s_emb: torch.Tensor,
-        p_emb: torch.Tensor,
-        o_emb: torch.Tensor,
-        combine: str,
-    ):
-        def PlattScaler(model):
-            # The scalars ωm1 and ωm0 in Equation 5 denote the learned weight and bias of the logistic regression (Platt-Scaler) for the model m.
-            bias = torch.Tensor([1,])
-            weight = torch.Tensor([1,]) 
-            score = model._scorer.score_emb(s_emb, p_emb, o_emb, combine)
-            return 1/(1+torch.exp(-(weight * score + bias)))
-
-        n = len(self.models)
-        scores = (1/n) + sum([PlattScaler(model) for model in self.models])
-
-        return scores
-
-    def load(self, models):
-        self.models = models
 
 class Ensemble(KgeModel):
     def __init__(
@@ -43,12 +15,74 @@ class Ensemble(KgeModel):
         super().__init__(
             config=config,
             dataset=dataset,
-            scorer=EnsembleScorer,
+            scorer=RelationalScorer,
             configuration_key=configuration_key,
             init_for_load_only=init_for_load_only,
         )
         self.models = []
 
-    def load(self, models):
+    def load(self, models, checkpoints):
         self.models = models
-        self._scorer.load(models)
+    
+    def PlattScaler(score):
+        # The scalars ωm1 and ωm0 in Equation 5 denote the learned weight and bias of the logistic regression (Platt-Scaler) for the model m.
+        bias = torch.Tensor([1,])
+        weight = torch.Tensor([1,])
+        return 1/(1+torch.exp(-(weight * score + bias)))
+
+    def score_spo(self, score_spos: Tensor, p: Tensor, o: Tensor, direction=None) -> Tensor:
+        scores = []
+        for model in self.models:
+            score = model.score_spo(s, p, o, direction)
+            scores.append(score)
+
+        n = len(self.models)
+        scores = (1/n) + sum([self.PlattScaler(score) for score in scores])
+    
+        return scores
+
+    def score_sp(self, s: Tensor, p: Tensor, o: Tensor = None) -> Tensor:
+        scores = []
+        for model in self.models:
+            score = model.score_sp(s, p, o)
+            scores.append(score)
+
+        n = len(self.models)
+        scores = (1/n) + sum([self.PlattScaler(score) for score in scores])
+    
+        return scores
+
+    def score_po(self, p: Tensor, o: Tensor, s: Tensor = None) -> Tensor:
+        scores = []
+        for model in self.models:
+            score = model.score_po(s, p, o)
+            scores.append(score)
+
+        n = len(self.models)
+        scores = (1/n) + sum([self.PlattScaler(score) for score in scores])
+    
+        return scores
+
+    def score_so(self, s: Tensor, o: Tensor, p: Tensor = None) -> Tensor:
+        scores = []
+        for model in self.models:
+            score = model.score_so(s, p, o)
+            scores.append(score)
+
+        n = len(self.models)
+        scores = (1/n) + sum([self.PlattScaler(score) for score in scores])
+    
+        return scores
+
+    def score_sp_po(
+        self, s: Tensor, p: Tensor, o: Tensor, entity_subset: Tensor = None
+    ) -> Tensor:
+        scores = []
+        for model in self.models:
+            score = model.score_sp_po(s, p, o, entity_subset)
+            scores.append(score)
+
+        n = len(self.models)
+        scores = (1/n) + sum([self.PlattScaler(score) for score in scores])
+    
+        return scores
